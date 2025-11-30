fd20b3da631d2d5443f213c4977f937eca8b5426
moltoabadsandra-netizen <moltoabadsandra@gmail.com>
2025-11-30 11:08:31 +0100

    Create PTGAS.java

---
+++ practica3/src/PTGAS.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Representa el personal tècnic i de gestió.
+ * 
+ *  - campus on treballa
+ */
+
+public class PTGAS extends Usuari {
+
+    // campus de treball
+
+
+    // capçalera del constructor
+    // public PTGAS(String alies, String correu, String campus);
+
+}
