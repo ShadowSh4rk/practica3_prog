544896fdcf91582ce42b1ff7c2b53227d55fcf06
moltoabadsandra-netizen <moltoabadsandra@gmail.com>
2025-11-30 11:11:07 +0100

    Create LlistaUsuaris.java

---
+++ practica3/src/LlistaUsuaris.java
@@ -0,0 +1,21 @@
+/**
+ * Classe de gestió de la llista d'usuaris.
+ * 
+ *
+ *  carregar usuaris des d'arxiu
+ *  cercar usuaris per alies
+ *  afegir nous usuaris
+ */
+
+public class LlistaUsuaris {
+
+    // estructura on es guardaran tots els usuaris registrats
+
+    // capçalera per afegir un usuari
+    // public void afegirUsuari(Usuari u);
+
+    // capçalera per buscar un usuari per alies
+    // public Usuari buscar(String alies);
+
+}
+
