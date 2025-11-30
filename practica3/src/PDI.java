7e2b7af1085efd806b80fae37816e9c432707b8e
moltoabadsandra-netizen <moltoabadsandra@gmail.com>
2025-11-30 11:07:58 +0100

    Create PDI.java

---
+++ practica3/src/PDI.java
@@ -0,0 +1,23 @@
+PDI.java
+/**
+ * 
+ * Representa el personal docent i investigador.
+ * 
+ * 
+ *  departament (DEIM, DEEEA, DEQ, ...)
+ *   campus on treballa
+ */
+
+public class PDI extends Usuari {
+
+    // departament
+
+    // campus de treball
+
+
+    // capçalera del constructor
+    // public PDI(String alies, String correu, String departament, String campus);
+
+
+    // mètodes específics per PDI si cal
+}
