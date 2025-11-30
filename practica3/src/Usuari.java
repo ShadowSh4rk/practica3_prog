1e0d1283a3e8295dc18a7c68af37e7160b55bd90
moltoabadsandra-netizen <moltoabadsandra@gmail.com>
2025-11-30 11:06:21 +0100

    Create Usuari.java

---
+++ practica3/src/Usuari.java
@@ -0,0 +1,26 @@
+/**
+ * Representa un usuari registrat que pot apuntar-se a activitats.
+ * 
+ * Atributs comuns:
+ *  - alies identificador
+ *  - correu institucional (fins abans de la @)
+ *
+ *  Només existeixen usuaris dels tipus PDI, PTGAS i Estudiant.
+ */
+
+public abstract class Usuari {
+
+    // alies identificador de l'usuari
+
+    // part del correu fins abans de @
+
+    // llista d'activitats a les quals està inscrit l'usuari
+
+
+    // capçalera del constructor
+    // public Usuari(String alies, String correu);
+
+    // getters i setters 
+
+    // mètodes comuns als usuaris si es necessiten
+}
