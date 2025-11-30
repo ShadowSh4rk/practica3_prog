0f33dfa64d70abad3f5a788094048f75b977e13d
moltoabadsandra-netizen <moltoabadsandra@gmail.com>
2025-11-30 11:09:05 +0100

    Create Estudiant.java

---
+++ practica3/src/Estudiant.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Representa un estudiant matriculat a un ensenyament URV.
+ * 
+*ensenyament (GEI, GESST, BioGEI, ...) 
+* any inici
+
+ *  
+ */
+
+public class Estudiant extends Usuari {
+
+    // ensenyament matriculat
+
+    // any d'inici dels estudis
+
+
+    // capçalera del constructor
+    // public Estudiant(String alies, String correu, String ensenyament, int anyInici);
+
+}
