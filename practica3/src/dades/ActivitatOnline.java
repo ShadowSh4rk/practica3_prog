60732d4e0bb0de5f103e1fe649650d8f7b3153d2
Salma El Boukre <salma.elboukre@estudiants.urv.cat>
2025-12-22 17:53:50 +0100

    Actualitzacio subclase ActivitatOnline (metodes, getters, correccions data)

--- practica3/src/dades/ActivitatOnline.java
+++ practica3/src/dades/ActivitatOnline.java
@@ -1,16 +1,26 @@
 package dades;
-
 import java.util.Date;
-
 public class ActivitatOnline extends Activitats {
     private Date dataInici;
     private int periodeVisualitzacio;
-    private String enllaç; 
+    private String enllac; 
 
-    public ActivitatOnline(String nom, String[]colectius, Date dataIniciInscripcio, Date dataFiInscripcio, Date dataInici, int periodeVisualitzacio, String enllaç) {
+    public ActivitatOnline(String nom, String[]colectius, Date dataIniciInscripcio, Date dataFiInscripcio, Date edataInici, int periodeVisualitzacio, String enllac) {
         super(nom, colectius, dataIniciInscripcio, dataFiInscripcio, "Online");
         this.dataInici = dataInici;
         this.periodeVisualitzacio = periodeVisualitzacio;
-        this.enllaç = enllaç; 
+        this.enllac = enllac; 
     }
+
+    @Override
+    public boolean teClasseAvui(Date avui){
+        return false; 
+    }
+
+    @Override
+    public boolean esActivaAvui(Date avui){
+        Date dataFi=new Date(dataInici.getDia()+periodeVisualitzacio, dataInici,getMes(), dataInici.getAny());
+dataInici.
+    }
+
 }
