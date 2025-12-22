bb07ef146a9edea542576ba0523561603e966247
Salma El Boukre <salma.elboukre@estudiants.urv.cat>
2025-12-22 18:00:12 +0100

    Actualitzacio subclase ActivitatUnDia (metodes, getters, correccions data)

--- practica3/src/dades/ActivitatUnDia.java
+++ practica3/src/dades/ActivitatUnDia.java
@@ -1,19 +1,44 @@
 package dades;
 
-import java.util.Date;
-
 public class ActivitatUnDia extends Activitats {
-    private Date data; 
-    //private LocalTime horari; 
+    private Data data; 
+    private String horari; 
     private String ciutat; 
     private double preu; 
 
-    public ActivitatUnDia(String nom, String[] colectius, Date dataIniciInscripcio, Date dataFiInscripcio, Date data, /*LocalTime horari,*/ String ciutat, int limitPlaces, double preu) {
+    public ActivitatUnDia(String nom, String[] colectius, Data dataIniciInscripcio, Data dataFiInscripcio, Data data, String horari, String ciutat, int limitPlaces, double preu) {
         super(nom, colectius, dataIniciInscripcio, dataFiInscripcio, "UnDia");
         this.data = data;
-        //this.horari = horari;
-        this.ciutat = ciutat;
-        this.limitPlaces = limitPlaces; 
+        this.horari = horari;
+        this.ciutat = ciutat; 
+        this.limitPlaces=limitPlaces;
         this.preu = preu; 
+    }
+
+    public String getCiutat(){
+        return ciutat; 
+    }
+
+    public String getHorari(){
+        return horari; 
+    }
+
+    public Data getData(){
+        return data; 
+    }
+
+    @Override
+    public boolean teClasseAvui(Data avui){
+        return avui.esIgual(data); 
+    }
+
+    @Override
+    public boolean esActivaAvui(Data avui){
+        return avui.esIgual(data);
+    }
+
+    @Override
+    public double getPreu(){
+        return preu; 
     }
 }
