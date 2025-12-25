49f737a9bb417889c552f8527f126ba331b32a80
Salma El Boukre <salma.elboukre@estudiants.urv.cat>
2025-12-25 11:55:28 +0100

    Actualitzacio ActivitatUnDia(actualitzada amb comentaris javadoc)

--- practica3/src/dades/ActivitatUnDia.java
+++ practica3/src/dades/ActivitatUnDia.java
@@ -1,11 +1,28 @@
 package dades;
 
+/**
+ * Representa una activitat que es realitza en un sol dia.
+ */
+
 public class ActivitatUnDia extends Activitats {
     private Data data; 
     private String horari; 
     private String ciutat; 
     private double preu; 
 
+     /**
+     * Constructor de l'activitat d'un dia.
+     *
+     * @param nom Nom de l'activitat
+     * @param colectius Col·lectius a qui s'ofereix (PDI, PTGAS, Estudiants)
+     * @param dataIniciInscripcio Data d'inici del període d'inscripció
+     * @param dataFiInscripcio Data final del període d'inscripció
+     * @param data Data en què es realitza l'activitat
+     * @param horari Horari de l'activitat
+     * @param ciutat Ciutat on es realitza
+     * @param limitPlaces Límits de places disponibles
+     * @param preu Preu de l'activitat
+     */
     public ActivitatUnDia(String nom, String[] colectius, Data dataIniciInscripcio, Data dataFiInscripcio, Data data, String horari, String ciutat, int limitPlaces, double preu) {
         super(nom, colectius, dataIniciInscripcio, dataFiInscripcio, "UnDia");
         this.data = data;
@@ -14,28 +31,58 @@ public class ActivitatUnDia extends Activitats {
         this.limitPlaces=limitPlaces;
         this.preu = preu; 
     }
-
+    /**
+     * Retorna la ciutat on es realitza l'activitat.
+     *
+     * @return Nom de la ciutat
+     */
     public String getCiutat(){
         return ciutat; 
     }
-
+    /**
+     * Retorna l'horari de l'activitat.
+     *
+     * @return Horari en format cadena (ex. "10:00-12:00")
+     */
     public String getHorari(){
         return horari; 
     }
 
+    /**
+     * Retorna la data en què es realitza l'activitat.
+     *
+     * @return Data de l'activitat
+     */ 
     public Data getData(){
         return data; 
     }
-
+    /**
+     * Indica si hi ha classe en la data indicada.
+     *
+     * @param avui Data a comprovar
+     * @return true si hi ha classe avui, false en cas contrari
+     */
     @Override
     public boolean teClasseAvui(Data avui){
         return avui.esIgual(data); 
     }
 
+    /**
+     * Indica si l'activitat està activa en la data indicada.
+     *
+     * @param avui Data a comprovar
+     * @return true si l'activitat està activa avui, false en cas contrari
+     */
     @Override
     public boolean esActivaAvui(Data avui){
         return avui.esIgual(data);
     }
+
+    /**
+     * Retorna el preu de l'activitat.
+     *
+     * @return Preu
+     */
 
     @Override
     public double getPreu(){
