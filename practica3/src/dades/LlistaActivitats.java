88b4ea1704e86ccffcf8595d248d838c822e11f1
Salma El Boukre <salma.elboukre@estudiants.urv.cat>
2026-01-04 18:47:10 +0100

    Actualitzacio LlistaActivitats(comentaris JavaDoc)

--- practica3/src/dades/LlistaActivitats.java
+++ practica3/src/dades/LlistaActivitats.java
@@ -6,20 +6,39 @@ import java.io.IOException;
 
 import excepcions.ActivitatInexistentException;
 
+/**
+ * Gestiona una llista d'activitats i ofereix funcionalitats
+ * per afegir, eliminar, buscar i filtrar activitats
+ */
 public class LlistaActivitats {
 
     private Activitats [] llista; 
     private int numActivitats; //index de la llista
     
+    /**
+     * Crea una llista d'activitats amb una mida màxima determinada.
+     * @param mida Mida maxima de la llista
+     */
     public LlistaActivitats(int mida){
         llista = new Activitats[mida];
         numActivitats=0; 
     }
 
+    /**
+     * Retorna el nombre d'activitats actuals a la llista
+     * 
+     * @return Nombre d'activitats
+     */
     public int getNumActivitats(){
         return numActivitats; 
     }
 
+    /**
+     * Retorna l'activitat en la posicio especificada
+     * @param i Index de l'activitat
+     * @return Activitats a la posicio i 
+     * @throws IndexOutOfBoundsException si l'index es fora de rang
+     */
     public Activitats getActivitat(int i){
         if(i<0 || i>= numActivitats){
             throw new IndexOutOfBoundsException("Posicio es troba fora dels limits.");
@@ -27,7 +46,14 @@ public class LlistaActivitats {
         return llista[i];
     }
 
-    //Afegir activitat ordenada per nom
+    /**
+     * Afegeix una activitat a la llista mantenint l'ordre alfabètic pel nom.
+     * També actualitza el fitxer de dades.
+     * 
+     * @param activitat Activitat a afegir
+     * @throws IOException Si hi ha un error escrivint el fitxer
+     */
+    
     public void afegir(Activitats activitat) throws IOException{
         if(numActivitats>=llista.length){
             throw new IllegalStateException("La llista d'activitats esta plena."); 
@@ -48,7 +74,14 @@ public class LlistaActivitats {
 
     }
 
-    // Eliminar activitat per posicio
+    /**
+     * Elimina una activitat de la llista segons la seva posició.
+     * També actualitza el fitxer de dades.
+     * 
+     * @param pos Posicio de l'activitat a eliminar
+     * @throws IOException Si hi ha un error escrivint el fitxer
+     * @throws IndexOutOfBoundsException Si la posicio es fora de rang
+     */
     public void eliminar(int pos) throws IOException{
         if(pos<0 || pos>= numActivitats){
             throw new IndexOutOfBoundsException("Posicio es troba fora dels limits.");
@@ -63,6 +96,12 @@ public class LlistaActivitats {
         escriuFitxerActivitats();
     }
 
+    /**
+     * Busca una activitat pel seu nom 
+     * 
+     * @param nom Nom de l'activitat
+     * @return Activitat trobada o null si no existeix 
+     */
     public Activitats buscarPerNom(String nom){
         for(int i=0; i<numActivitats; i++){
             if(llista[i].getNom().equalsIgnoreCase(nom)){
@@ -72,6 +111,13 @@ public class LlistaActivitats {
         return null; 
     }
 
+    /**
+     * Obte una activitat pel nom i llença excepcio si no existeix 
+     * 
+     * @param nom Nom de l'activitat
+     * @return Activitat trobada 
+     * @throws ActivitatInexistentException Si l'activitat no existeix
+     */
     public Activitats obtenirPerNom(String nom) throws ActivitatInexistentException{
         Activitats a = buscarPerNom(nom);
         if(a==null){
@@ -80,7 +126,12 @@ public class LlistaActivitats {
         return a; 
     }
 
-    //Activitats en periode d'inscripcio
+    /**
+     * Retona un array amb les activitats que estan en periode d'inscripcio
+     * 
+     * @param avui Data actual
+     * @return Array d'activitats disponibles per inscriure's avui 
+     */
     public Activitats[] activitatsEnInscripcio(Data avui){
         Activitats [] res = new Activitats[numActivitats];
         int j=0; 
@@ -93,7 +144,13 @@ public class LlistaActivitats {
         return res; 
     }
 
-    //Activitats actives avui
+    /**
+     * Retorna un array amb les activitat que estan actives avui 
+     * 
+     * @param avui Data actuual 
+     * @return Array d'activitats actives avui 
+     */
+
     public Activitats [] activitatsActivesAvui(Data avui){
         Activitats [] res= new Activitats[numActivitats]; 
         int j=0; 
@@ -106,7 +163,12 @@ public class LlistaActivitats {
         return res; 
     }
 
-    //Activitats amb classe avui
+    /**
+     * Retorna un array amb les activitats que tenen classe avui 
+     * 
+     * @param avui Data actual 
+     * @return Array d'activitats amb classe avui 
+     */
     public Activitats [] activitatsAmbClasseAvui(Data avui){
         Activitats [] res = new Activitats[numActivitats];
         int j=0; 
@@ -119,6 +181,11 @@ public class LlistaActivitats {
         return res; 
     }
 
+    /**
+     * Retorna un array amb les activitats que encara tenen places disponibles
+     * 
+     * @return Array d'activitats amb places disponibles
+     */
     public Activitats [] activitatsAmbPlaces(){
         Activitats [] res = new Activitats[numActivitats];
         int j=0; 
@@ -131,6 +198,11 @@ public class LlistaActivitats {
         return res; 
     }
 
+    /**
+     * Escriu la llista completa d'activitats en un fitxer de text.
+     * 
+     * @throws IOException Si hi ha un error en escriure el fitxer
+     */
     public void escriuFitxerActivitats() throws IOException{
         BufferedWriter escriptura = new BufferedWriter(new FileWriter("FitxerLlistaActivitats.txt"));
 
@@ -141,6 +213,12 @@ public class LlistaActivitats {
         escriptura.close();
     }
 
+    /**
+     * Comprova si hi ha alguna activitat el dia indicat
+     * 
+     * @param data Data a comprovar
+     * @return true si hi ha alguna activitat aquell dia, false altrament 
+     */
     public boolean hiHaActivitat(Data data) {
 
         boolean trobat = false;
